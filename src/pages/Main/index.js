683cd180ed5c9d165bdeb19398dfd4b00a687d1e
Thiago Kienbaum <thiago.kienbaum@hotmail.com>
2020-07-19 18:04:55 -0300

    Add styles to main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import { Title } from './styles'
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa'
+import api from '../../services/api'
+import { Container, Form, SubmitButton } from './styles'
 
-function Main() {
-    return <Title>Hello World</Title>;
+export default class Main extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            newRepository: '',
+            repositories: [],
+            loading: false,
+        };
+    }
+
+    handleInputChange = event => {
+        this.setState({ newRepository: event.target.value })
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        this.setState({ loading: true });
+
+        const { newRepository, repositories } = this.state;
+        const response = await api.get(`/repos/${newRepository}`)
+
+        const data = {
+            name: response.data.full_name,
+        };
+
+        this.setState({
+            repositories: [...repositories, data],
+            newRepository: '',
+            loading: false,
+        })
+    };
+
+    render() {
+        const { newRepository, loading } = this.state;
+
+        return (
+        <Container>
+            <h1>
+                <FaGithubAlt />
+                Repositories
+            </h1>
+
+            <Form onSubmit={ this.handleSubmit }>
+                <input
+                    type="text"
+                    placeholder="Add repository"
+                    value={ newRepository }
+                    onChange={ this.handleInputChange }
+                />
+                <SubmitButton loading={loading}>
+                    {loading ? <FaSpinner color="#FFF" size={14} /> : <FaPlus color="#FFF" size={14} />}
+
+                </SubmitButton>
+            </Form>
+        </Container>
+    );}
 };
-
-export default Main;
